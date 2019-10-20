cedce97087f8a204a5724dea102e12622909cc42
Oscar Fraxedas <ofraxedas@microdea.com>
2019-10-20 18:06:32 -0400

    Running test :-)

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#Java
+brew cask install java
+
+#Composer
+brew install gcc
+brew install composer
+brew install php@7.3
+brew link php@7.3
+
+#Webdriver
+composer require facebook/webdriver
+curl http://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar -o driver/selenium-server-standalone-3.141.59.jar
+
+
---
+++ start.sh
@@ -0,0 +1,2 @@
+echo "Open http://localhost:4444/wd/hub/ in your browser"
+java -jar driver/selenium-server-standalone-3.141.59.jar
